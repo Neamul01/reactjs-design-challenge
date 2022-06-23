58031a56c794ca5a4deec6b1896816cbdbb5a1e8
Noman01 <neamulhaque2002@gmail.com>
2022-06-23 18:48:55 +0600

    new component created and tailwind installed

---
+++ src/components/Calender.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calender = () => {
+    return (
+        <div className='text-4xl'>
+            this is calender component
+        </div>
+    );
+};
+
+export default Calender;
